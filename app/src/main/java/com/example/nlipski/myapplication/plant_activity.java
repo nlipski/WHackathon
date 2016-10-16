5b6c6b76bd9405a46746b16ba0700220373c83a7
Nlipski <13ml76@queensu.ca>
2016-10-16 08:40:35 -0400

    12345

--- app/src/main/java/com/example/nlipski/myapplication/plant_activity.java
+++ app/src/main/java/com/example/nlipski/myapplication/plant_activity.java
@@ -8,11 +8,14 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 
 public class plant_activity extends AppCompatActivity {
     Button b1,b2,b3;
+    ImageView im;
     PopupWindow popupWindow;
     LayoutInflater layoutInflater;
     RelativeLayout relativeLayout;
@@ -22,14 +25,51 @@ public class plant_activity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_activity);
+        im=(ImageView)findViewById(R.id.ivPlant) ;
         b1=(Button)findViewById(R.id.btnWater);
+        im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.description_popup, null);
+                popupWindow = new PopupWindow(container, 300, 300, true);
+                popupWindow.showAtLocation(relativeLayout, Gravity.NO_GRAVITY, 400, 400);
+                container.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+
+                });
+            }
+            });
+
         b1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 layoutInflater=(LayoutInflater)getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
                 ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.water_popup,null);
-                popupWindow =new PopupWindow(container,400,400,true);
-                popupWindow.showAtLocation(relativeLayout, Gravity.NO_GRAVITY,500,500);
+                popupWindow =new PopupWindow(container,300,300,true);
+                popupWindow.showAtLocation(relativeLayout, Gravity.NO_GRAVITY,400,400);
+                container.setOnTouchListener(new View.OnTouchListener(){
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+
+                });
+            }
+        });
+        b2=(Button)findViewById(R.id.btnWater);
+        b2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                layoutInflater=(LayoutInflater)getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.sunlight_popup,null);
+                popupWindow =new PopupWindow(container,300,300,true);
+                popupWindow.showAtLocation(relativeLayout, Gravity.NO_GRAVITY,400,400);
                 container.setOnTouchListener(new View.OnTouchListener(){
                     @Override
                     public boolean onTouch(View view, MotionEvent motionEvent) {
